b66a9aa60f81effc3149bccfe821a3db51ad56cd
DawsonEnsley <dawsonlensley@gmail.com>
2022-03-14 11:08:15 -0500

    Notes on strings and lists pt.1

--- notes.py
+++ notes.py
@@ -1,173 +1,79 @@
-
-#   Create Name
-# weaver = turtle.Turtle()
-#   Draw Chain or Links
-#   Create Variables
-# links = [1, 2, 3, 4, 5, 6]
-# sides = [1, 2, 3, 4, 5, 6]
-#   Create Parameters
-# weaver.width(5)
-# weaver.color('orange')
-#   Move back so the chain is centered.
-# weaver.penup()
-# weaver.back(80)
-# weaver.pendown()
-# Create Loop Expression
-# for chain in links:
-#   Draw a Hexagon
-# for shape in sides:
-#   weaver.forward(50)
-#   weaver.right(60)
-#   weaver.speed(5)
-#   Scoot over to the next link.
-# weaver.penup()
-# weaver.forward(20)
-# weaver.left(60)
-# weaver.pendown()
-
-#   Draw Line Skips
-# lane = turtle.Turtle()
-# lane.hideturtle()
-#   Make the width thicker
-# lane.width(5)
-#   Move back without drawing
-# lane.penup()
-# lane.left(90)
-# lane.forward(5)
-# lane.right(90)
-# lane.back(100)
-# lane.pendown()
-#   Draw a red line
-# lane.color("red")
-# lane.forward(50)
-#   Move forward without drawing
-# lane.penup()
-# lane.forward(50)
-# lane.pendown()
-#   Draw an orange line
-# lane.color("orange")
-# lane.forward(50)
-#   Move forward without drawing
-# lane.penup()
-# lane.forward(50)
-# lane.pendown()
-#   Draw a yellow line
-# lane.color("yellow")
-# lane.forward(50)
-
-#   Draw Lines with Loop
-# for lines in [1, 2, 3]:
-#   lane.forward.(50)
-#   lane.penup()
-#   lane.forward(50)
-#   lane.pendown()
-
-#   Draw Lines with Change in color in For Loop
-# for diff_color in ["red", "orange", "yellow"]:
-#   lane.color(diff_color)
-
-#   Draw Shape instead of Line with Nested Loop
-
-# Lane = turtle.Turtle()
-# Lane.hideturtle()
-# Lane.width(5)
-# Lane.penup()
-# Lane.back(100)
-# Lane.pendown()
-# Lane.showturtle()
-
-# for diff_color in ["green", "blue", "purple"]:
-#    Lane.color(diff_color)
-#    for square in [1, 2, 3, 4]:
-#         Lane.forward(50)
-#         Lane.right(90)
-#     Lane.penup()
-#     Lane.forward(100)
-#     Lane.pendown()
-
-# paul = turtle.Turtle()
-# sides = [1, 2, 3, 4]
-# paul.width(5)
-# paul.penup()
-# paul.right(90)
-# paul.forward(50)
-# paul.left(90)
-# paul.back(25)
-# paul.pendown()
-
-# for large in sides:
-#     paul.forward(100)
-#     paul.right(90)
-#     for small in sides:
-#         paul.forward(10)
-#         paul.right(90)
-
-# stair = turtle.Turtle()
+# def word_pyramid(word):
+#     length = len(word)
+#     for n in range(length):
+#         print(word[0:n + 1])
 #
-# stair.speed(5)
-# stair.color("green")
-# stair.width(3)
 #
-# for steps in range(15):
-#     stair.forward(10)
+# word_pyramid("abracadabra")
+#  Use in interactive Terminal
 #
-#     if steps % 2 == 0:
-#         stair.left(90)
+#
+# def word_triangle(word):
+#     length = len(word)
+#     for n in range(length):
+#         print(word[:length - n])
+#
+#
+# word_triangle("abracadabra")
+#  Use in interactive Terminal
+
+# Concatenation Example
+# name = input("What's your name?\n\n")
+# print("\nHello, ", name, "! It's nice to meet you.\n")
+# color = input("What's your favorite color?\n\n")
+# print("\nWow!", "I love the color", color, "!")
+
+# F-strings Example (No Concatenation)
+# name = input("What's your name?\n\n")
+# print(f"\nHi, {name}! It's very nice to meet you.\n")
+# color = input("What's your favorite color?\n\n")
+# print(f"\nAh, {color}, what a lovely color!")
+
+# f-strings Example: Use of int in strings
+# n1 = int(input("Enter a number: "))
+# n2 = int(input("Enter another number: "))
+# n3 = int(input("Enter a third number: "))
+# sum = int(n1) + int(n2) + int(n3)
+# print(f'{n1} + {n2} + {n3} = {sum}')
+
+# starts-with Example: parameters
+# def starts_with(s1, s2):
+#     if s1[0] == s2[0]:
+#         return True
+#     if s1[0] != s2[0]:
+#         return False
+
+# print(starts_with("banana", "bread"))
+#
+# print(starts_with("zamboni", "kiwi"))
+
+# starts_with Example: Length of a string
+# def starts_with(long, short):
+#     for position in range(len(short)):
+#         if long[position] != short[position]:
+#             return False
+#     return True
+
+# print(starts_with("apple", "app"))
+#
+# print(starts_with("manatee", "mango"))
+
+# starts_with Example: Slicing
+# def starts_with(long, short):
+#     if long[0:len(short)] == short:
+#         return True
 #     else:
-#         stair.right(90)
+#         return False
 
-import turtle
-t = turtle.Turtle()
-s = turtle.Turtle()
+# print(starts_with("apple", "app"))
+#
+# print(starts_with("manatee", "mango"))
 
-
-def tri_spiral(length, turn, color):
-    t.color(color)
-    t.width(3)
-    t.speed(7)
-    for side in range(length):
-        t.forward(side * 10)
-        t.right(turn)
-
-
-tri_spiral(22, 120, "green")
-
-
-def spiral(sides, turn, color, width):
-    s.color(color)
-    s.width(width)
-    s.speed(7)
-    for n in range(sides):
-        s.forward(n)
-        s.right(turn)
-
-
-spiral(50, 30, "gold", 3)
-
-
-def star(color, sides, length, angle, distance):
-    galileo = turtle.Turtle()
-    galileo.color(color)  # colorful!
-    galileo.width(5)  # visible!
-    galileo.speed(0)  # fast!
-    galileo.penup()
-    galileo.left(angle)  # away from center
-    galileo.forward(distance)
-    galileo.pendown()  # start drawing
-    for side in range(sides):
-        galileo.forward(length)
-        galileo.left(720 / sides)
-    galileo.hideturtle()  # just the star
-
-
-for angle in [180, 135, 90, 45, 0]:
-    star("red", 5, 60, angle, 100)
-
-for angle in [180, 135, 90, 45, 0]:
-    star("blue", 5, 30, angle, 50)
-
-for angle in [180, 135, 90, 45, 0]:
-    star("white", 5, 15, angle, 25)
-
-
-turtle.exitonclick()
+# Boolean Example
+# def good_length(n):
+#     return len(n) < 7 or len(n) < 65
+#
+#
+# print(good_length("apple"))
+#
+# print(good_length("hippopotamus"))
