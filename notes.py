903f3ea7fa15a546128c6b99728231aa9ef47790
DawsonEnsley <dawsonlensley@gmail.com>
2022-03-20 15:57:24 -0500

    Elevator Game

--- notes.py
+++ notes.py
@@ -1,3 +1,5 @@
+import time
+items = []
 # def word_pyramid(word):
 #     length = len(word)
 #     for n in range(length):
@@ -108,3 +110,94 @@
 #     print(n)
 #     n -= 1
 # print('Blastoff!')
+
+
+def print_sleep(print_pause):
+    print(print_pause)
+    time.sleep(1)
+
+
+def intro():
+    print_sleep("You have just arrived at your new job!")
+    print_sleep("You are in the elevator.")
+
+
+def first_floor(items):
+    print_sleep("You push the button for the first floor.")
+    print_sleep("After a few moments, you find yourself in the Lobby.")
+    if "ID card" in items:
+        print_sleep("The clerk greets you, but she has already given you your"
+                    " ID card, so there is nothing more to do here now.")
+    else:
+        print_sleep("The clerk greets you and gives you your ID card.")
+        items.append("ID card")
+    print_sleep("You head back to the elevator.")
+    choose_floor(items)
+
+
+def second_floor(items):
+    print_sleep("You push the button for the second floor.")
+    print_sleep("After a few moments, you find yourself in "
+                "the human resources department.")
+    if "handbook" in items:
+        print_sleep("The HR folks are busy at their desks.")
+        print_sleep("There doesn't seem to be much to do here.")
+    else:
+        print_sleep("The head of HR greets you.")
+        if "ID card" in items:
+            print_sleep("He looks at your ID card and then gives"
+                        " you a copy of the employee handbook.")
+            items.append("handbook")
+        else:
+            print_sleep("He has something for you, but says he can't "
+                        "give it to you until you go get your ID card.")
+    print_sleep("You head back to the elevator.")
+    choose_floor(items)
+
+
+def third_floor(items):
+    print_sleep("You push the button for the third floor.")
+    print_sleep("After a few moments, you find yourself in "
+                "the engineering department.")
+    print_sleep("This is where you work!")
+    if "ID card" in items:
+        print_sleep("You use your ID card to open the door.")
+        print_sleep("Your program manager greets you and tells "
+                    "you that you need to have a copy of the employee "
+                    "handbook in order to start work.")
+        if "handbook" in items:
+            print_sleep("Fortunately, you got that from HR!")
+            print_sleep("Congratulations! You are ready to start your new job"
+                        " as vice president of engineering!")
+        else:
+            print_sleep("They scowl when they see that you don't have it, "
+                        "and send you back to the elevator.")
+            choose_floor(items)
+    else:
+        print_sleep("Unfortunately, the door is locked and you can't get in.")
+        print_sleep("It looks like you need some kind of key card to open the door.")
+        print_sleep("You head back to the elevator.")
+
+    choose_floor(items)
+
+
+def choose_floor(items):
+    print_sleep("Please enter the number for the floor you would like to visit.")
+    floor = input("1. Lobby\n"
+                  "2. Human resources\n"
+                  "3. Engineering department\n")
+    if floor == '1':
+        first_floor(items)
+    elif floor == '2':
+        second_floor(items)
+    elif floor == '3':
+        third_floor(items)
+
+
+def play_game():
+    items = []
+    intro()
+    choose_floor(items)
+
+
+play_game()
